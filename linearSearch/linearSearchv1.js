ce49bb3b6febed1f7a629cc124375069bd540c7a
ankit025 <ankitjha1276@gmail.com>
2020-12-01 18:48:43 +0530

    linearSearch

--- linearSearch/linearSearchv1.js
+++ linearSearch/linearSearchv1.js
@@ -1,3 +1,14 @@
+/*
+In computer science, a linear search or sequential search is a method for finding an element within a list. 
+It sequentially checks each element of the list until a match is found or the whole list has been searched.
+The time complexity of the linear search algorithm is O(n). 
+
+Steps
+--> Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+--> If x matches with an element, return the index.
+--> If x doesn’t match with any of elements, return -1.
+*/
+
 var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 function linearFunction(myArr, elementToFind) {
@@ -10,4 +21,11 @@ function linearFunction(myArr, elementToFind) {
 }
 
 let result = linearFunction(rainbow, "orange");
-console.log(result);
+console.log(result,"result");
+
+function linearFunctionv2(myArr, elementToFind) {
+  return myArr.indexOf(elementToFind);
+}
+
+let result1 = linearFunctionv2(rainbow, "orange");
+console.log(result1,"result1");
--- linearSearch/linearSearchv2.js
+++
@@ -1,7 +0,0 @@
-var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-function linearFunction(myArr, elementToFind) {
- return myArr.indexOf(elementToFind)
-}
-
-let result = linearFunction(rainbow, "orange");
-console.log(result);
